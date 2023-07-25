887b35872b086358099c11e9a89d017f35ac49ff
itasworkshop <itasworkshop@gmail.com>
2023-07-25 13:26:37 +0530

    25 July

---
+++ notes/concepts/loops.py
@@ -0,0 +1,62 @@
+
+mylist  = [1,2,3,5,6,9]
+
+"""
+for i in mylist:
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+
+for i in range(5):
+    print("hello world!")
+
+
+for i in range(10):
+    print("hello world!",i)
+"""
+sum = 0
+
+for i in range(5):
+    sum = sum+i
+
+print(sum)
+
+mylist = [1, 2, 4, 6]
+
+result = 0
+for i in mylist:
+    result = result + i
+
+print(result)
+
+
+mylist1 = [1,34,22,56,78,33,12]
+newlist = []
+
+for i in mylist1:
+    if (i%2 == 0):
+        newlist.append(i)
+
+print(newlist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes/concepts/loops1.py
@@ -0,0 +1,12 @@
+""" while loop
+count= 0
+
+while(count<5):
+    print("Hello World!",count)
+    count = count+1
+"""
+count= 5
+
+while(count>0):
+    print("Hello World!",count)
+    count = count-1
